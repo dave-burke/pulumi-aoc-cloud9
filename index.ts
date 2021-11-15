d201441e7373fa76dedcc84278718e0c0237605a
Dave Burke <dburke84@gmail.com>
2021-11-14 20:52:38 -0600

    Add owner policy

--- index.ts
+++ index.ts
@@ -2,4 +2,8 @@ import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 
 const owner = new aws.iam.User('aoc-owner');
+new aws.iam.UserPolicyAttachment('aoc-owner-policy', {
+    user: owner.name,
+    policyArn: 'arn:aws:iam::aws:policy/AWSCloud9Administrator',
+});
 
